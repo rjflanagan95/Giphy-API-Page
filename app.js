7e92a081ea6b338389e3ec9cab896a1e791525cb
Robert Flanagan <robertflanagan95@gmail.com>
2018-11-01 20:16:24 -0400

    gifs start/stop when clicked

--- app.js
+++ app.js
@@ -1,51 +1,53 @@
 themeArray = ["lord of the rings", "star wars", "harry potter", "avengers"];
 
-for (var i = 0; i < themeArray.length; i++) {
-    var newButton = $("<button>");
-    newButton.addClass("gif-button");
-    newButton.attr("id", themeArray[i]);
-    newButton.text(themeArray[i]);
-    $("#button-area").append(newButton);
+function refreshButtons() {
+    for (var i = 0; i < themeArray.length; i++) {
+        var newButton = $("<button>");
+        newButton.addClass("gif-button");
+        newButton.attr("id", themeArray[i]);
+        newButton.text(themeArray[i]);
+        $("#button-area").append(newButton);
+    }
 }
 
-$(".gif-button").click(function() {
+$(document).on("click", ".gif-button", function() {
     // clear the gif area for new gifs
     $("#gif-area").empty();
 
     // value of button clicked, which will be added to queryURL
     var searchTerm = $(this).attr("id");
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + searchTerm;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + searchTerm;
 
-    for (var i = 0; i < 10; i++) {
-        // call ajax
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }) .then(function(response) {
-            // console.log(response.data);
+    // call ajax
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }) .then(function(response) {
+        for (var i = 0; i < 10; i++) {
+            var shortcut = response.data[i];
             var newGifBox = $("<div>");
             newGifBox.addClass("gif-box");
             var newGif = $("<img>");
             newGif.addClass("gif");
-            newGif.attr("data-still", response.data.images.fixed_height_still.url);
-            newGif.attr("data-animate", response.data.images.fixed_height.url);
+            newGif.attr("data-still", shortcut.images.fixed_height_still.url);
+            newGif.attr("data-animate", shortcut.images.fixed_height.url);
             newGif.attr("status", "still");
-            newGif.attr("src", response.data.images.fixed_height_still.url);
+            newGif.attr("src", shortcut.images.fixed_height_still.url);
             newGifBox.append(newGif);
+            newGifBox.append("<br><span class='rating'>" + shortcut.rating + "</span>");
             $("#gif-area").prepend(newGifBox);
-        });
-    }
-    
+        }
+    });
 });
 
-$(".gif").click(function() {
-    console.log("clicked a gif");
-    console.log($(this));
+$(document).on("click", ".gif", function() {
     if ($(this).attr("status") === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
+        $(this).attr("status", "animate");
     } else {
         $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");;
+        $(this).attr("status", "still");;
     }
 });
+
+refreshButtons();
